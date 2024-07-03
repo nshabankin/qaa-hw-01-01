eff2e803772bd92652fc1ae7b24648678c6cd504
nshabankin <n.shabankin@gmail.com>
2024-07-03 17:11:52 +0300

    Add autotests with JUnit5 and keep the legacy tests with JUnit 4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -31,4 +31,28 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    public void ShouldReturnProperRemainIfAboveBoundaryJUnitAPI() {
+        int amount = 2_600;
+        int expected = 400;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ShouldReturnProperRemainIfBelowBoundaryJUnitAPI() {
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ShouldReturnProperRemainIfEqualsBoundaryJUnitAPI() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
 }
