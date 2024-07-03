037a12cd2530cea9140b08a2e6e12b7159e3b45e
nshabankin <n.shabankin@gmail.com>
2024-07-03 16:56:11 +0300

    Add autotests with JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void ShouldReturnProperRemainIfAboveBoundary() {
+        int amount = 2_600;
+        int expected = 400;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldReturnProperRemainIfBelowBoundary() {
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldReturnProperRemainIfEqualsBoundary() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+}
